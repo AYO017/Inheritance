677bba590050fb3079d2355f17b2fffe65a0ce9f
MR Jad <Ayo@192.168.0.124>
2018-12-27 17:38:58 +0000

    fin

--- inheritance_Test/src/inheritance_Test/Calculation.java
+++ inheritance_Test/src/inheritance_Test/Calculation.java
@@ -12,6 +12,6 @@ public class Calculation {
 	public void Sub(int x, int y ){
 		
 		cal = x - y;
-		System.out.println("the difference of the given number is :"+cal	);
+		System.out.println("the difference of the given number is :"+cal);
 	}
 }
